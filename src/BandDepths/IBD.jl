bc3c089dc4c3a1b2e97f843e80812e4d31c96a63
z-vig <zacharymvig@gmail.com>
2024-01-12 11:48:07 -0500

    Small fix

--- src/BandDepths/IBD.jl
+++ src/BandDepths/IBD.jl
@@ -6,7 +6,7 @@ export IBD_map
 This module allows the mapping of integrated band depths by specifying the begining wavelength, ending wavelength and the integration step size
 """
 
-function IBD_map(image::Array{<:AbstractFloat,3}continuum::Array{<:AbstractFloat,3},λvec::Vector{Float64},λ₁::Real,λ₂::Real,step::Int)
+function IBD_map(image::Array{<:AbstractFloat,3},continuum::Array{<:AbstractFloat,3},λvec::Vector{Float64},λ₁::Real,λ₂::Real,step::Int)
     println("Hello World!")
 end
 
